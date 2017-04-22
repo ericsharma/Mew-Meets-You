0a9c403fa227f90ea9f5c888ef86ae162aac4557
Liz Chhouk <kisara.of.pern@gmail.com>
2017-04-22 14:47:01 -0400

    Update 20170422180304_create_pokemons.rb

--- db/migrate/20170422180304_create_pokemons.rb
+++ db/migrate/20170422180304_create_pokemons.rb
@@ -2,7 +2,7 @@ class CreatePokemons < ActiveRecord::Migration[5.0]
   def change
     create_table :pokemons do |t|
       t.string :name, null: false
-      t.string :type 
+      t.string :type
       t.integer :weight, null: false
       t.string :image_url, null: false
       
