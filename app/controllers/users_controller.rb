2f14adbccacd8b70128482c38ee977648d945b51
ericsharma <sharma.e@husky.neu.edu>
2017-04-22 16:51:28 -0400

    fixed routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
+    binding.pry
     @user = User.find(params[:id])
     @matches = @user.pokemons
+  end
+
+  def destroy
+    redirect_to destroy_user_session_path, method: :delete
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resource :home
 
-  resources :users
+  resources :users, only: [:show]
 
   devise_for :users
 
