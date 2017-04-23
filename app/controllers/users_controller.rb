55a158951fb71e84f77f069943284dd3a08e2e29
Liz Chhouk <kisaras.playground@gmail.com>
2017-04-23 00:14:55 -0400

    final tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.email = params[:user][:email]
+    @user.avatar_url = params[:user][:avatar_url]
+
+    if @user.save
+      flash[:notice]='Success! Your profile has been updated.'
+      redirect_to @user
+    else
+      @user.email = params[:user][:email]
+      @user.avatar_url = params[:user][:avatar_url]
+      render :edit
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#new'
-    get '/users/sign_in' => 'devise/sessions#destroy'
-    post '/users/sign_in' => 'devise/sessions#create'
-  end
 
-  resource :home
-  resources :users, only: [:show, :edit, :update]
+  devise_for :users
 
   namespace :api do
     namespace :v1 do
@@ -15,4 +9,5 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :users, only: [:show, :edit, :update]
 end
